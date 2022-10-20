8a817debd683fa64fa74e4cdf3ace00d7f446c60
zekothedev <z.erassan@gmail.com>
2022-10-20 20:24:10 +0200

    Add Module1

---
+++ Module1.py
@@ -0,0 +1,48 @@
+import datetime
+import random
+
+stations = ['Groningen', 'Rotterdam', 'Tilburg']
+station = random.choice(stations)
+
+naamGebruiker = input('Voer uw naam in (Druk op "Enter" om anoniem door te gaan): ')
+
+if len(naamGebruiker) < 2:
+    naamGebruiker = 'Anoniem'
+
+else:
+    naamGebruiker = naamGebruiker
+ 
+while True:
+    vastgelegdeTijd = datetime.datetime.now()
+
+    bericht = input('Vul uw bericht in (max 140 karakters): ')
+    tijdVanBericht = f"{vastgelegdeTijd.year}-{vastgelegdeTijd.month}-{vastgelegdeTijd.day} {vastgelegdeTijd.hour}:{vastgelegdeTijd.minute}"
+
+    if len(bericht) > 140:
+        print('Uw bericht bevat meer dan 140 Karakters')
+        continue
+    else:
+        break
+
+with open('database.txt', 'a+') as database:
+    database.seek(0)
+    data = database.read(100)
+    if len(data) > 0:
+        database.write('\n')
+    database.write(f'{bericht};{tijdVanBericht};{naamGebruiker};{station}')
+    database.close()
+
+
+
+        # print(f'Bericht: {bericht}')
+        # print(f'Tijdstip: {tijdVanBericht}')
+        # print(f'Naam: {naamGebruiker}')
+        # print(f'Station: {station}')
+
+
+
+
+
+
+
+
